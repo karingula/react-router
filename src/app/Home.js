99b72db2e0e36fa40db5f02858961d3fccb6d66d
karingula <karingula@users.noreply.github.com>
2018-03-09 11:35:59 -0500

    params

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+} from 'react-router-dom';
+
+const Child = ({ match }) => console.log('match-property', match) || (
+  <div>
+    <h2>ID: {match.params.id}</h2>
+  </div>
+)
 
 export class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <h2>Accounts</h2>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/courses">Courses</Link></li>
+            <li><Link to="/netflix">Netflix</Link></li>
+            <li><Link to="/zillow">Zillow Group</Link></li>
+            <li><Link to="/yahoo">Yahoo</Link></li>
+            <li><Link to="/modus-create">Modus Create</Link></li>
           </ul>
 
-          <hr/>
-
-          <Route  exact path="/" component={Home} />
-          <Route path="/about" component={About}/>
-          <Route path="/courses" component={Topics}/>
+          <Route path='/:id' component={Child}/>
         </div>
       </Router>
+
     )
   }
 }
