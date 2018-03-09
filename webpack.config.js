01384d45ab385b87d7b474217add7cabfd9d2603
karingula <karingula@users.noreply.github.com>
2018-03-09 08:56:48 -0500

    dynamic routing

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-  entry = SRC_DIR + "/app/index.js";
+  entry: SRC_DIR + "/app/index.js",
   output: {
     path: DIST_DIR + "/app",
     filename: "bundle.js",
     publicPath: "/app"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test:/.\js?/,
         include: SRC_DIR,
@@ -23,6 +23,7 @@ var config = {
       }
     ]
   },
+  mode: "development",
 };
 
 module.exports = config;
